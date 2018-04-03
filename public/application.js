8b7a9b7721bfa44d950e188365ad8eec42d27ab9
James Logue <logueja@gmail.com>
2018-04-03 10:55:12 -0600

    Emit custom message from client side

--- public/application.js
+++ public/application.js
@@ -6,4 +6,23 @@ socket.on('connect', () => {
 
 socket.on('message', message => {
   console.log('something came along on the "message" channel', message)
+  addMessage(message)
+  socket.send({
+    username: 'bob loblaw',
+    text: 'check out law blog'
+  })
 })
+
+window.addEventListener('keyup', () => {
+  socket.emit('panda', {name: 'test'})
+})
+
+const addMessage = message => {
+  const messages = document.querySelector('.messages')
+  const newMessage = document.createElement('div')
+  newMessage.innerHTML = `
+    <h2>something came along on the "message" channel</h2>
+    <p>${message}</p>
+    `
+  messages.appendChild(newMessage)
+}
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
-const path = require('path');
+const path = require('path')
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/', function (req, res){
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 io.on('connection', socket => {
   console.log('Someone has connected')
-  socket.emit('message', `A new user, ${Date.now()}, has connected.`)
+  socket.emit('message', `A new user, ${Date.now()}, has connected`)
+
+  socket.on('message', message => {
+    console.log(`the new user's name is ${message.username}, and their message is ${message.text}`)
+  })
+
+  socket.on('panda', panda => {
+    console.log(`the panda's name is ${panda.name}`)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('a user has disconnected')
+  })
 })
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('Your server is up and running on Port 3000. Good job!');
+  console.log('Your server is up and running on Port 3000. Good job!')
 });
