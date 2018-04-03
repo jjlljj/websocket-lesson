ffe131813976abb2853e38cfcc89e96c91f7779d
James Logue <logueja@gmail.com>
2018-04-03 10:12:40 -0600

    Initial setup

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+io.on('connection', socket => {
+  console.log('Someone has connected')
+})
+
 http.listen(process.env.PORT || 3000, function(){
   console.log('Your server is up and running on Port 3000. Good job!');
 });
